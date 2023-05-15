ec243c65f3aa742aff6b5afe9944669da7f53f12
Santosh Jogdand <santoshjogdand256@gmail.com>
2023-05-15 23:51:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const API_KEY = "b9d26520f7b8a11536f284acef2aa108";
-
+let CityName;
+let temperature;
+let weatherText;
 function getWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
   fetch(url)
@@ -10,20 +12,27 @@ function getWeather(city) {
         weather: [weather]
       } = data;
       const icon = `https://openweathermap.org/img/wn/${weather.icon}.png`;
-      const weatherText = weather.description;
-      const temperature  = parseInt(temp - 273.123);
-      console.log(temp);
-      const CityName = (
+       weatherText = weather.description;
+       temperature  = parseInt(temp - 273.123);
+       CityName = (
           (city[0]).toUpperCase()+(city.slice(1)).toLowerCase()
         );
-      console.log(CityName);
+          
+        
+          
       document.getElementById("weather").innerHTML = `
         
       <img src="${icon}" alt="Weather icon">
-        <p>In <b>${CityName}</b> there is a <b>${weatherText}</b> and tempreture is <b>${temperature}°C</b>.</p>
+        <p>In <b>${CityName}</b> there is a <b>${weatherText}</b> and temprature is <b>${temperature}°C</b>.</p>
         
       `;
     });
+}
+function wa(){
+  let phno = prompt("Enter Phone no");
+  let msg = "In *"+ CityName+ "* there is a *" + weatherText +"* and temprature is *"+ temperature +"*°C";
+  console.log(msg);
+  document.getElementById("an").href =`https://wa.me/91${phno}?text=${msg}`
 }
 
 document.querySelector("form").addEventListener("submit", (e) => {
